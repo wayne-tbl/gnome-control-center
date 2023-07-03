fd166d1d2b17018c248611500e44d82fc7f5e522
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2023-07-03 20:53:02 +0300

    FIXME: power: Disable automatic suspend options

--- panels/power/cc-power-panel.c
+++ panels/power/cc-power-panel.c
@@ -769,6 +769,7 @@ setup_power_saving (CcPowerPanel *self)
 
   setup_blank_screen_rows (self);
 
+#if 0
   /* The default values for these settings are unfortunate for us;
    * timeout == 0, action == suspend means 'do nothing' - just
    * as timout === anything, action == nothing.
@@ -806,6 +807,7 @@ setup_power_saving (CcPowerPanel *self)
 
       set_ac_battery_ui_mode (self);
     }
+#endif
 }
 
 static const char *
