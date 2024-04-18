c7d8510c448170f2c1fa6ce987dd3230aef72e6d
Bardia Moshiri <fakeshell@bardia.tech>
2024-04-17 20:50:41 -0400

    panels: add andromeda settings panel
    * implement a switch for sharing a directory with guest
    * add a toggle for NFC access
    * add a buttons to clear app data and kill an app
    * add a bottom sheet for app specific actions
    * add a toggle for notification passthrough service
    
    Co-authored-by: Jesús Higueras <jesus@furilabs.com>
    Signed-off-by: Bardia Moshiri <fakeshell@bardia.tech>

--- shell/cc-panel-loader.c
+++ shell/cc-panel-loader.c
@@ -52,6 +52,7 @@ extern GType cc_online_accounts_panel_get_type (void);
 extern GType cc_power_panel_get_type (void);
 extern GType cc_printers_panel_get_type (void);
 extern GType cc_privacy_panel_get_type (void);
+extern GType cc_andromeda_panel_get_type (void);
 extern GType cc_usb_panel_get_type (void);
 extern GType cc_search_panel_get_type (void);
 extern GType cc_sharing_panel_get_type (void);
@@ -109,6 +110,7 @@ static CcPanelLoaderVtable default_panels[] =
   PANEL_TYPE("printers",         cc_printers_panel_get_type,             NULL),
   PANEL_TYPE("privacy",          cc_privacy_panel_get_type,              NULL),
 //  PANEL_TYPE("search",           cc_search_panel_get_type,               NULL),
+  PANEL_TYPE("andromeda",         cc_andromeda_panel_get_type,             NULL),
   PANEL_TYPE("usb",              cc_usb_panel_get_type,                  NULL),
   PANEL_TYPE("sharing",          cc_sharing_panel_get_type,              cc_sharing_panel_static_init_func),
   PANEL_TYPE("sound",            cc_sound_panel_get_type,                NULL),
