a6d6042e6677d1d2e47c855e76abfae0aa953b09
Bardia Moshiri <bardia@furilabs.com>
2025-10-17 20:19:50 -0400

    SQUASHME: wwan: make use of gobject props in mms dialog
    should be squashed into 36a841663699530ea217013c5cc880659d937d32 during GNOME 50 rebase
    
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- panels/wwan/cc-wwan-mms-dialog.c
+++ panels/wwan/cc-wwan-mms-dialog.c
@@ -41,11 +41,13 @@ struct _CcWwanMmsDialog
   GDBusProxy        *mmsd_proxy;
   GDBusProxy        *context_proxy;
 
-  gchar             *current_proxy;
-  gchar             *current_center;
-  gchar             *current_apn;
-  gchar             *current_username;
-  gchar             *current_password;
+  gchar             *message_proxy;
+  gchar             *message_center;
+  gchar             *access_point_name;
+  gchar             *username;
+  gchar             *password;
+
+  gint               pending_operations;
 
   GtkButton         *save_button;
   AdwEntryRow       *message_proxy_row;
@@ -53,11 +55,95 @@ struct _CcWwanMmsDialog
   AdwEntryRow       *access_point_row;
   AdwEntryRow       *username_row;
   AdwEntryRow       *password_row;
-
-  gint               pending_operations;
 };
 
 G_DEFINE_TYPE (CcWwanMmsDialog, cc_wwan_mms_dialog, GTK_TYPE_DIALOG)
+
+enum {
+  PROP_0,
+  PROP_PORT_NAME,
+  PROP_MMS_CONTEXT_PATH,
+  PROP_MMSD_PROXY,
+  PROP_CONTEXT_PROXY,
+  PROP_MESSAGE_PROXY,
+  PROP_MESSAGE_CENTER,
+  PROP_ACCESS_POINT_NAME,
+  PROP_USERNAME,
+  PROP_PASSWORD,
+  N_PROPS
+};
+
+static GParamSpec *properties[N_PROPS];
+
+static void
+cc_wwan_mms_dialog_set_message_proxy (CcWwanMmsDialog *self,
+                                      const gchar     *message_proxy)
+{
+  g_return_if_fail (CC_IS_WWAN_MMS_DIALOG (self));
+
+  if (g_strcmp0 (self->message_proxy, message_proxy) == 0)
+    return;
+
+  g_free (self->message_proxy);
+  self->message_proxy = g_strdup (message_proxy);
+  g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_MESSAGE_PROXY]);
+}
+
+static void
+cc_wwan_mms_dialog_set_message_center (CcWwanMmsDialog *self,
+                                       const gchar     *message_center)
+{
+  g_return_if_fail (CC_IS_WWAN_MMS_DIALOG (self));
+
+  if (g_strcmp0 (self->message_center, message_center) == 0)
+    return;
+
+  g_free (self->message_center);
+  self->message_center = g_strdup (message_center);
+  g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_MESSAGE_CENTER]);
+}
+
+static void
+cc_wwan_mms_dialog_set_access_point_name (CcWwanMmsDialog *self,
+                                          const gchar     *access_point_name)
+{
+  g_return_if_fail (CC_IS_WWAN_MMS_DIALOG (self));
+
+  if (g_strcmp0 (self->access_point_name, access_point_name) == 0)
+    return;
+
+  g_free (self->access_point_name);
+  self->access_point_name = g_strdup (access_point_name);
+  g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_ACCESS_POINT_NAME]);
+}
+
+static void
+cc_wwan_mms_dialog_set_username (CcWwanMmsDialog *self,
+                                 const gchar     *username)
+{
+  g_return_if_fail (CC_IS_WWAN_MMS_DIALOG (self));
+
+  if (g_strcmp0 (self->username, username) == 0)
+    return;
+
+  g_free (self->username);
+  self->username = g_strdup (username);
+  g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_USERNAME]);
+}
+
+static void
+cc_wwan_mms_dialog_set_password (CcWwanMmsDialog *self,
+                                 const gchar     *password)
+{
+  g_return_if_fail (CC_IS_WWAN_MMS_DIALOG (self));
+
+  if (g_strcmp0 (self->password, password) == 0)
+    return;
+
+  g_free (self->password);
+  self->password = g_strdup (password);
+  g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_PASSWORD]);
+}
 
 static char *
 find_mms_context_path (const char *modem_path)
@@ -151,6 +237,23 @@ find_mms_context_path (const char *modem_path)
 }
 
 static void
+update_ui_from_properties (CcWwanMmsDialog *self)
+{
+  g_return_if_fail (CC_IS_WWAN_MMS_DIALOG (self));
+
+  if (self->message_proxy)
+    gtk_editable_set_text (GTK_EDITABLE (self->message_proxy_row), self->message_proxy);
+  if (self->message_center)
+    gtk_editable_set_text (GTK_EDITABLE (self->message_center_row), self->message_center);
+  if (self->access_point_name)
+    gtk_editable_set_text (GTK_EDITABLE (self->access_point_row), self->access_point_name);
+  if (self->username)
+    gtk_editable_set_text (GTK_EDITABLE (self->username_row), self->username);
+  if (self->password)
+    gtk_editable_set_text (GTK_EDITABLE (self->password_row), self->password);
+}
+
+static void
 on_property_set (GObject      *source_object,
                  GAsyncResult *res,
                  gpointer      user_data)
@@ -171,7 +274,7 @@ on_property_set (GObject      *source_object,
 
   self->pending_operations--;
   if (self->pending_operations <= 0) {
-    self->pending_operations = 0; /* Safety: ensure it never goes negative */
+    self->pending_operations = 0;
     gtk_widget_set_sensitive (GTK_WIDGET (self->save_button), TRUE);
     if (!error)
       gtk_window_close (GTK_WINDOW (self));
@@ -201,44 +304,38 @@ on_properties_received (GObject      *source_object,
   value = g_variant_lookup_value (dict, "MessageProxy", G_VARIANT_TYPE_STRING);
   if (value) {
     str_value = g_variant_get_string (value, NULL);
-    gtk_editable_set_text (GTK_EDITABLE (self->message_proxy_row), str_value);
-    g_free (self->current_proxy);
-    self->current_proxy = g_strdup (str_value);
+    cc_wwan_mms_dialog_set_message_proxy (self, str_value);
     g_clear_pointer (&value, g_variant_unref);
   }
 
   value = g_variant_lookup_value (dict, "MessageCenter", G_VARIANT_TYPE_STRING);
   if (value) {
     str_value = g_variant_get_string (value, NULL);
-    gtk_editable_set_text (GTK_EDITABLE (self->message_center_row), str_value);
-    g_free (self->current_center);
-    self->current_center = g_strdup (str_value);
+    cc_wwan_mms_dialog_set_message_center (self, str_value);
     g_clear_pointer (&value, g_variant_unref);
   }
 
   value = g_variant_lookup_value (dict, "AccessPointName", G_VARIANT_TYPE_STRING);
   if (value) {
     str_value = g_variant_get_string (value, NULL);
-    gtk_editable_set_text (GTK_EDITABLE (self->access_point_row), str_value);
-    g_free (self->current_apn);
-    self->current_apn = g_strdup (str_value);
+    cc_wwan_mms_dialog_set_access_point_name (self, str_value);
+    g_clear_pointer (&value, g_variant_unref);
   }
 
   value = g_variant_lookup_value (dict, "Username", G_VARIANT_TYPE_STRING);
   if (value) {
     str_value = g_variant_get_string (value, NULL);
-    gtk_editable_set_text (GTK_EDITABLE (self->username_row), str_value);
-    g_free (self->current_username);
-    self->current_username = g_strdup (str_value);
+    cc_wwan_mms_dialog_set_username (self, str_value);
+    g_clear_pointer (&value, g_variant_unref);
   }
 
   value = g_variant_lookup_value (dict, "Password", G_VARIANT_TYPE_STRING);
   if (value) {
     str_value = g_variant_get_string (value, NULL);
-    gtk_editable_set_text (GTK_EDITABLE (self->password_row), str_value);
-    g_free (self->current_password);
-    self->current_password = g_strdup (str_value);
+    cc_wwan_mms_dialog_set_password (self, str_value);
   }
+
+  update_ui_from_properties (self);
 }
 
 static void
@@ -255,7 +352,8 @@ on_context_proxy_acquired (GObject      *source_object,
     return;
   }
 
-  /* Get the initial properties */
+  g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_CONTEXT_PROXY]);
+
   g_dbus_proxy_call (self->context_proxy,
                      "GetProperties",
                      NULL,
@@ -279,6 +377,8 @@ on_mmsd_proxy_acquired (GObject      *source_object,
     g_warning ("Failed to create MMSD proxy: %s", error->message);
     return;
   }
+
+  g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_MMSD_PROXY]);
 }
 
 static void
@@ -357,27 +457,27 @@ on_save_clicked (GtkButton       *button,
 
   g_variant_builder_init (&builder, G_VARIANT_TYPE ("a{ss}"));
 
-  if (g_strcmp0 (proxy, self->current_proxy) != 0) {
+  if (g_strcmp0 (proxy, self->message_proxy) != 0) {
     g_variant_builder_add (&builder, "{ss}", "MessageProxy", proxy);
     changes_made = TRUE;
   }
 
-  if (g_strcmp0 (center, self->current_center) != 0) {
+  if (g_strcmp0 (center, self->message_center) != 0) {
     g_variant_builder_add (&builder, "{ss}", "MessageCenter", center);
     changes_made = TRUE;
   }
 
-  if (g_strcmp0 (apn, self->current_apn) != 0) {
+  if (g_strcmp0 (apn, self->access_point_name) != 0) {
     g_variant_builder_add (&builder, "{ss}", "AccessPointName", apn);
     changes_made = TRUE;
   }
 
-  if (g_strcmp0 (username, self->current_username) != 0) {
+  if (g_strcmp0 (username, self->username) != 0) {
     g_variant_builder_add (&builder, "{ss}", "Username", username);
     changes_made = TRUE;
   }
 
-  if (g_strcmp0 (password, self->current_password) != 0) {
+  if (g_strcmp0 (password, self->password) != 0) {
     g_variant_builder_add (&builder, "{ss}", "Password", password);
     changes_made = TRUE;
   }
@@ -385,7 +485,6 @@ on_save_clicked (GtkButton       *button,
   if (changes_made) {
     GVariant *properties = g_variant_builder_end (&builder);
     set_mms_context_properties_async (self, properties);
-    g_variant_unref (properties);
   } else {
     gtk_widget_set_sensitive (GTK_WIDGET (button), TRUE);
     gtk_window_close (GTK_WINDOW (self));
@@ -394,40 +493,137 @@ on_save_clicked (GtkButton       *button,
 }
 
 static void
-cc_wwan_mms_dialog_dispose (GObject *object)
+cc_wwan_mms_dialog_constructed (GObject *object)
 {
   CcWwanMmsDialog *self = CC_WWAN_MMS_DIALOG (object);
 
+  G_OBJECT_CLASS (cc_wwan_mms_dialog_parent_class)->constructed (object);
+
+  if (!self->port_name || !g_variant_is_object_path (self->port_name)) {
+    g_warning ("Invalid modem path: %s", self->port_name);
+    return;
+  }
+
+  /* Find the MMS context path */
+  self->mms_context_path = find_mms_context_path (self->port_name);
+  if (!self->mms_context_path) {
+    g_warning ("Failed to find MMS context path");
+    return;
+  }
+
+  g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_MMS_CONTEXT_PATH]);
+
+  /* Set up both proxies */
+  setup_context_proxy (self);
+  setup_mmsd_proxy (self);
+}
+
+static void
+cc_wwan_mms_dialog_set_property (GObject      *object,
+                                 guint         prop_id,
+                                 const GValue *value,
+                                 GParamSpec   *pspec)
+{
+  CcWwanMmsDialog *self = CC_WWAN_MMS_DIALOG (object);
+
+  switch (prop_id)
+    {
+    case PROP_PORT_NAME:
+      g_free (self->port_name);
+      self->port_name = g_value_dup_string (value);
+      break;
+
+    case PROP_MESSAGE_PROXY:
+      cc_wwan_mms_dialog_set_message_proxy (self, g_value_get_string (value));
+      break;
+
+    case PROP_MESSAGE_CENTER:
+      cc_wwan_mms_dialog_set_message_center (self, g_value_get_string (value));
+      break;
+
+    case PROP_ACCESS_POINT_NAME:
+      cc_wwan_mms_dialog_set_access_point_name (self, g_value_get_string (value));
+      break;
+
+    case PROP_USERNAME:
+      cc_wwan_mms_dialog_set_username (self, g_value_get_string (value));
+      break;
+
+    case PROP_PASSWORD:
+      cc_wwan_mms_dialog_set_password (self, g_value_get_string (value));
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
+
+static void
+cc_wwan_mms_dialog_get_property (GObject    *object,
+                                 guint       prop_id,
+                                 GValue     *value,
+                                 GParamSpec *pspec)
+{
+  CcWwanMmsDialog *self = CC_WWAN_MMS_DIALOG (object);
+
+  switch (prop_id)
+    {
+    case PROP_PORT_NAME:
+      g_value_set_string (value, self->port_name);
+      break;
+
+    case PROP_MMS_CONTEXT_PATH:
+      g_value_set_string (value, self->mms_context_path);
+      break;
+
+    case PROP_MMSD_PROXY:
+      g_value_set_object (value, self->mmsd_proxy);
+      break;
+
+    case PROP_CONTEXT_PROXY:
+      g_value_set_object (value, self->context_proxy);
+      break;
+
+    case PROP_MESSAGE_PROXY:
+      g_value_set_string (value, self->message_proxy);
+      break;
+
+    case PROP_MESSAGE_CENTER:
+      g_value_set_string (value, self->message_center);
+      break;
+
+    case PROP_ACCESS_POINT_NAME:
+      g_value_set_string (value, self->access_point_name);
+      break;
+
+    case PROP_USERNAME:
+      g_value_set_string (value, self->username);
+      break;
+
+    case PROP_PASSWORD:
+      g_value_set_string (value, self->password);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
+
+static void
+cc_wwan_mms_dialog_dispose (GObject *object)
+{
+  CcWwanMmsDialog *self = CC_WWAN_MMS_DIALOG (object);
   g_clear_object (&self->mmsd_proxy);
   g_clear_object (&self->context_proxy);
-  g_clear_pointer (&self->current_proxy, g_free);
-  g_clear_pointer (&self->current_center, g_free);
-  g_clear_pointer (&self->current_apn, g_free);
-  g_clear_pointer (&self->current_username, g_free);
-  g_clear_pointer (&self->current_password, g_free);
+  g_clear_pointer (&self->message_proxy, g_free);
+  g_clear_pointer (&self->message_center, g_free);
+  g_clear_pointer (&self->access_point_name, g_free);
+  g_clear_pointer (&self->username, g_free);
+  g_clear_pointer (&self->password, g_free);
   g_clear_pointer (&self->port_name, g_free);
   g_clear_pointer (&self->mms_context_path, g_free);
 
   G_OBJECT_CLASS (cc_wwan_mms_dialog_parent_class)->dispose (object);
-}
-
-static void
-cc_wwan_mms_dialog_finalize (GObject *object)
-{
-  CcWwanMmsDialog *self = CC_WWAN_MMS_DIALOG (object);
-
-  /* Ensure we're fully cleaned up */
-  g_clear_object (&self->mmsd_proxy);
-  g_clear_object (&self->context_proxy);
-  g_clear_pointer (&self->current_proxy, g_free);
-  g_clear_pointer (&self->current_center, g_free);
-  g_clear_pointer (&self->current_apn, g_free);
-  g_clear_pointer (&self->current_username, g_free);
-  g_clear_pointer (&self->current_password, g_free);
-  g_clear_pointer (&self->port_name, g_free);
-  g_clear_pointer (&self->mms_context_path, g_free);
-
-  G_OBJECT_CLASS (cc_wwan_mms_dialog_parent_class)->finalize (object);
 }
 
 static void
@@ -437,7 +633,74 @@ cc_wwan_mms_dialog_class_init (CcWwanMmsDialogClass *klass)
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
   object_class->dispose = cc_wwan_mms_dialog_dispose;
-  object_class->finalize = cc_wwan_mms_dialog_finalize;
+  object_class->get_property = cc_wwan_mms_dialog_get_property;
+  object_class->set_property = cc_wwan_mms_dialog_set_property;
+  object_class->constructed = cc_wwan_mms_dialog_constructed;
+
+  properties[PROP_PORT_NAME] =
+    g_param_spec_string ("port-name",
+                         "Port Name",
+                         "The modem port name/path",
+                         NULL,
+                         G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS);
+
+  properties[PROP_MMS_CONTEXT_PATH] =
+    g_param_spec_string ("mms-context-path",
+                         "MMS Context Path",
+                         "The DBus path for the MMS context",
+                         NULL,
+                         G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+
+  properties[PROP_MMSD_PROXY] =
+    g_param_spec_object ("mmsd-proxy",
+                         "MMSD Proxy",
+                         "The DBus proxy for the MMSD manager",
+                         G_TYPE_DBUS_PROXY,
+                         G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+
+  properties[PROP_CONTEXT_PROXY] =
+    g_param_spec_object ("context-proxy",
+                         "Context Proxy",
+                         "The DBus proxy for the connection context",
+                         G_TYPE_DBUS_PROXY,
+                         G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+
+  properties[PROP_MESSAGE_PROXY] =
+    g_param_spec_string ("message-proxy",
+                         "Message Proxy",
+                         "The MMS message proxy URL",
+                         NULL,
+                         G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+
+  properties[PROP_MESSAGE_CENTER] =
+    g_param_spec_string ("message-center",
+                         "Message Center",
+                         "The MMS message center URL",
+                         NULL,
+                         G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+
+  properties[PROP_ACCESS_POINT_NAME] =
+    g_param_spec_string ("access-point-name",
+                         "Access Point Name",
+                         "The MMS access point name (APN)",
+                         NULL,
+                         G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+
+  properties[PROP_USERNAME] =
+    g_param_spec_string ("username",
+                         "Username",
+                         "The MMS connection username",
+                         NULL,
+                         G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+
+  properties[PROP_PASSWORD] =
+    g_param_spec_string ("password",
+                         "Password",
+                         "The MMS connection password",
+                         NULL,
+                         G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+
+  g_object_class_install_properties (object_class, N_PROPS, properties);
 
   gtk_widget_class_set_template_from_resource (widget_class,
                                                "/org/gnome/control-center/wwan/cc-wwan-mms-dialog.ui");
@@ -455,60 +718,21 @@ cc_wwan_mms_dialog_init (CcWwanMmsDialog *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
 
-  self->current_proxy = NULL;
-  self->current_center = NULL;
-  self->current_apn = NULL;
-  self->current_username = NULL;
-  self->current_password = NULL;
-  self->mmsd_proxy = NULL;
-  self->context_proxy = NULL;
-  self->port_name = NULL;
-  self->mms_context_path = NULL;
   self->pending_operations = 0;
 
   g_signal_connect (self->save_button, "clicked", G_CALLBACK (on_save_clicked), self);
-}
-
-static void
-cc_wwan_mms_dialog_setup (CcWwanMmsDialog *self,
-                          const gchar     *port_name)
-{
-  g_return_if_fail (CC_IS_WWAN_MMS_DIALOG (self));
-  g_return_if_fail (port_name != NULL);
-
-  self->port_name = g_strdup (port_name);
-  if (!g_variant_is_object_path (self->port_name)) {
-    g_warning ("Invalid modem path: %s", self->port_name);
-    return;
-  }
-
-  /* Then find the MMS context path */
-  self->mms_context_path = find_mms_context_path (self->port_name);
-  if (!self->mms_context_path) {
-    g_warning ("Failed to find MMS context path");
-    return;
-  }
-
-  /* Set up both proxies */
-  setup_context_proxy (self);
-  setup_mmsd_proxy (self);
 }
 
 GtkWindow *
 cc_wwan_mms_dialog_new (GtkWindow   *parent_window,
                         const gchar *port_name)
 {
-  CcWwanMmsDialog *dialog;
-
   g_return_val_if_fail (GTK_IS_WINDOW (parent_window), NULL);
   g_return_val_if_fail (port_name != NULL, NULL);
 
-  dialog = g_object_new (CC_TYPE_WWAN_MMS_DIALOG,
-                         "transient-for", parent_window,
-                         "use-header-bar", 1,
-                         NULL);
-
-  cc_wwan_mms_dialog_setup (dialog, port_name);
-
-  return GTK_WINDOW (dialog);
+  return g_object_new (CC_TYPE_WWAN_MMS_DIALOG,
+                       "transient-for", parent_window,
+                       "use-header-bar", 1,
+                       "port-name", port_name,
+                       NULL);
 }
